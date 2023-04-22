0d9cbf9536caa14caf9543d3d63b2edac46fc49a
1709archie <agarwalarchie17@gmail.com>
2023-04-22 14:40:34 +0530

    Upload successful

--- upload.js
+++ upload.js
@@ -2,8 +2,8 @@ let droparea = document.getElementById("dropbox");
 var form = document.getElementById("form");
 const fileinput = document.getElementById("fileInput");
 const uploadbtn = document.getElementById("upload_btn");
-const host="http://localhost:5500"
-const uploadURL=`${host}/api/file_sharing/test`
+const host="https://filesharing-blgk.onrender.com"
+const uploadURL=`${host}/test`
 const FILE = document.getElementById("fileinput");
 const UPLOAD = document.getElementById("button");
 const SUBMIT = document.getElementById("submitfile");
@@ -34,39 +34,40 @@ droparea.addEventListener("drop", (e) => {
   console.log(files);
   if (files.length) {
     fileinput.files = files;
-    // uploadfiles();
+    uploadfiles();
   }
 });
 
 fileinput.addEventListener("change", () => {
-  // uploadfiles();
+  uploadfiles();
 });
 document.getElementById("button").addEventListener("click", (e) => {
   fileinput.click();
 });
-// function uploadfiles() {
-//   var file = fileinput.files[0];
-//   console.log(file);
-//   var formData=new FormData(droparea);
+function uploadfiles() {
+  var file = fileinput.files[0];
+  console.log(file);
+  console.log(file);
+  var formData=new FormData(droparea);
  
-//   console.log(formData.entries());
-//   formData.append('myfile', JSON.stringify(file[0]));
+  console.log(formData.entries());
+  formData.append('myfile', JSON.stringify(file[0]));
  
-//   console.log(formData.entries());
-//   const xhr = new XMLHttpRequest;
-//   xhr.onreadystatechange = (e) => {
-//     if (xhr.readyState === xhr.DONE  && xhr.status==200) {
-//       console.log(xhr.readyState);
-//         }
-//   }
-//   xhr.open('POST', uploadURL, true);
-//   xhr.setRequestHeader("Access-Control-Allow-Headers", "*");
-//   xhr.setRequestHeader("Access-Control-Allow-Methods", "*");
-//   console.log(formData.entries());
-//   xhr.send(formData);
-// };
-// const showLink=({file:url})=>{
-//   console.log(url);
-//   textvalue.value=url
-// }
+  console.log(formData.entries());
+  const xhr = new XMLHttpRequest;
+  xhr.onreadystatechange = (e) => {
+    if (xhr.readyState === xhr.DONE  && xhr.status==200) {
+      console.log(xhr.readyState);
+        }
+  }
+  xhr.open('POST', uploadURL, true);
+  xhr.setRequestHeader("Access-Control-Allow-Headers", "*");
+  xhr.setRequestHeader("Access-Control-Allow-Methods", "*");
+  console.log(formData.entries());
+  xhr.send(formData);
+};
+const showLink=({file:url})=>{
+  console.log(url);
+  textvalue.value=url
+}
 
